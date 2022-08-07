f20d287c2bf5c5759776e4ba46fc387dabca532a
tnwatts <tnwatts@live.com>
2022-08-07 14:50:26 -0600

    commiting before bootstrap install

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('what', views.what, name='what')
+    path('what', views.what, name='what'),
+    path('rocks', views.rocks_index, name='index'),
 ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Rock:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, category, description, grams):
+    self.name = name
+    self.category = category
+    self.description = description
+    self.grams = grams
+
+rocks = [
+  Rock('Granny', 'Igneous', 'super old granite rock, set in her ways, stubborn but cares about the young rocks', 35),
+  Rock('Rockette', 'Metamorphic', 'workhorse, used to be shale, now living the slate life, works hard and parties hard', 47),
+  Rock('Carl', 'Sedimentary', 'very bland limestone rock, unstable personality, doesn\'t like other rocks', 22),
+]
+
+
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def what(request):
     return render(request, 'what.html')
+
+def rocks_index(request):
+    return render(request, 'rocks/index.html', {'rocks' : rocks})
